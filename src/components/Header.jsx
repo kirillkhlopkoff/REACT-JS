1bb97af9e7fadf7334fed2be8dbb2bb06be170b7
kirillkhlopkoff <114176969+kirillkhlopkoff@users.noreply.github.com>
2023-07-12 16:10:33 +0300

    Приложение на Реакт, уроки IT-Kamasutra, сделали первые компоненты

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://www.freepnglogos.com/uploads/coffee-logo-png/coffee-logo-design-creative-idea-logo-elements-2.png" />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img  src ='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
+    </div>
+    <div>
+      avatar
+      <img src='https://imgv3.fotor.com/images/cover-photo-image/a-beautiful-girl-with-gray-hair-and-lucxy-neckless-generated-by-Fotor-AI.jpg' />
+    </div>
+    <div>
+      My posts
+    </div>
+    <div>
+      another
+    </div>
+    <div>
+      another
+    </div>
+  </div>
+}
+
+export default Profile;
